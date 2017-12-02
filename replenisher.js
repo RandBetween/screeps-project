ed2cf49f2f79cf10261c7213c5d8e1445a3fc6b9
Joe <c09joe.mercurio@gmail.com>
2017-12-02 17:16:05 -0500

    adjusted replenisher.js to move resources to arbritary # of links

--- replenisher.js
+++ replenisher.js
@@ -45,12 +45,19 @@ module.exports = function (creep, workerNum) {
         };
         
         // Add link to target array
-        
-        var link = Game.getObjectById("5a1dfd9971050f57ee51a8c3");
-        if (link.energy < link.energyCapacity) {
-            targets.push(link);
-        };
-    
+
+        var links = creep.room.find(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_LINK &&
+                structure.energy < structure.energyCapacity &&
+                structure.id != "5a1e0745f8b2a13832ecff45");
+            }
+        });
+
+        if (links.length > 0) {
+            targets.push(links[0]);
+        }
+           
         /** Withdraw phase for hauler creep **/
         if (creep.memory.deliverPhase == false && creep.carry.energy < creep.carryCapacity) {
             if(creep.withdraw(source, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
