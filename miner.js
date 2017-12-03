893fdf6838b30ba8e44689803af0ecaa8463a88c
Joe <c09joe.mercurio@gmail.com>
2017-12-03 00:24:16 -0500

    modified miner.js to no longer have to haul his minerals.

--- miner.js
+++ miner.js
@@ -4,34 +4,9 @@ module.exports = function (creep) {
         var y = 31;
     
         var source = Game.getObjectById("5873c16763ad7a7555b7b03b");
-        var storage = Game.getObjectById("5a19c44d9bc92518b838df2f");
     
-        if (creep.memory.harvestPhase == true && _.sum(creep.carry) < creep.carryCapacity) {
-    
-          var dropenergy = creep.pos.findClosestByPath(FIND_DROPPED_ENERGY, {
-              filter: (d) => {return (d.resourceType == RESOURCE_HYDROGEN)}
-          });
-    
-          if (dropenergy) {
-            if (creep.pickup(dropenergy) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(dropenergy);
-            }
-          } else if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(x,y);
-          }
-    
-        } else if (creep.memory.harvestPhase == true && creep.carry[RESOURCE_HYDROGEN] == creep.carryCapacity) {
-          creep.memory.harvestPhase = false;
-    
-        } else if (creep.memory.harvestPhase == false && creep.carry[RESOURCE_HYDROGEN] > 0) {
-    
-          if(creep.transfer(storage, RESOURCE_HYDROGEN) == ERR_NOT_IN_RANGE) {
-              creep.moveTo(storage);
-          }
-    
-        } else if (creep.memory.harvestPhase == false) {
-          creep.memory.harvestPhase = true;
+        if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(x, y);
         }
-    
-    }
+    };
     
