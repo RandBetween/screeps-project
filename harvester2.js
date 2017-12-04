0a61dfd9a347ccac7ea0f535a2299fff1aa7c968
Joe <c09joe.mercurio@gmail.com>
2017-12-03 20:33:02 -0500

    added mult-room support.

--- harvester2.js
+++ harvester2.js
@@ -6,15 +6,33 @@ module.exports = function (creep, room) {
     if (room == "W75N83") {
         x = 40;
         y = 42;
+
     } else if (room == "W76N83") {
         x = 29;
         y = 35;
     }
     
-    var sources = creep.room.find(FIND_SOURCES);
+    if (room == "W75N83") {
+        var sources = creep.room.find(FIND_SOURCES);
+        
+        if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(x, y);
+        }
 
-    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(x, y);
+    } else if (room == "W76N83") {
+        var sources = creep.room.find(FIND_SOURCES);
+        var target = Game.getObjectById("5a239349b45ebf5e61484b8f");
+        
+        if (creep.carry.energy < creep.carryCapacity) {
+            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(x, y);
+            }
+        } else {
+            if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(target);
+            }                
+        }
+
     }
     
 };
