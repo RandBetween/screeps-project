c2a438c6504d8c1ac204543fca42cd9b238e7dd6
Joe <c09joe.mercurio@gmail.com>
2021-04-21 07:58:00 -0400

    typo on hauler2 source object id

--- miner2.js
+++ miner2.js
@@ -3,7 +3,7 @@ module.exports = function (creep) {
     var x = 11;
     var y = 34;
 
-    var source = Game.getObjectById("55982fdbab097071b4adbfc31");
+    var source = Game.getObjectById("5982fdbab097071b4adbfc31");
 
     if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
         creep.moveTo(x, y);
